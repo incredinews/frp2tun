0afb5781ffdfaa7cc7efa3a6d863953f2ec68437
sudo-bencho <182349246+sudo-bencho@users.noreply.github.com>
2025-09-21 05:06:41 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,9 @@
 test -e /setup.sh && source /setup.sh
 
 #!/bin/ash
-
-echo starting
-test -e /etc/connector.conf  && /usr/bin/connector --config /etc/connector.conf   2>&1 |grep -v -e decryp -e key -e keepalive -e ndshake -e TUN -e Interface -e ncryp &
-test -e /etc/connector2.conf && /usr/bin/connector --config /etc/connector2.conf  2>&1 |grep -v -e decryp -e key -e keepalive -e ndshake -e TUN -e Interface -e ncryp &
-[[ -z "$AUTH" ]] || /usr/bin/chi server --port 4444 --reverse --backend http://127.0.0.1:8000 & 
-test -e /etc/caddyfile && caddy run --config /etc/caddyfile &
-/usr/bin/frps -c /opt/frps.toml
+echo ...
+test -e /etc/connector.conf  && ( while (true);do  /usr/bin/connector --config /etc/connector.conf   2>&1 |grep -v -e decryp -e key -e keepalive -e ndshake -e TUN -e Interface -e ncryp ;done ) &
+test -e /etc/connector2.conf && ( while (true);do  /usr/bin/connector --config /etc/connector2.conf  2>&1 |grep -v -e decryp -e key -e keepalive -e ndshake -e TUN -e Interface -e ncryp ;done ) &
+[[ -z "$AUTH" ]] || (while (true);do /usr/bin/chi server --port 4444 --reverse --backend http://127.0.0.1:7500 ;done) & 
+test -e /etc/caddyfile && (while (true);do caddy run --config /etc/caddyfile ;sleep 10;done ) &
+while (true);do /usr/bin/frps -c /opt/frps.toml;sleep 5 ;done
